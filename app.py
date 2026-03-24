b093899abfcd21ae7673eb0fe9d5cfd6b01ac318
tonniteles <tonniteles@gmail.com>
2026-03-24 17:58:09 -0300

    App Update

--- app.py
+++ app.py
@@ -1,60 +1,82 @@
 import API_OltCloud
 import streamlit as st
-import json
 
-api = API_OltCloud.OltCloudAPI()
+def get_api():
+    return API_OltCloud.OltCloudAPI()
+
+def get_search_type(option):
+    return {
+        'Número do Contrato': 'device_alias',
+        'Número de Série': 'serial',
+        'Endereço MAC': 'mac'
+    }.get(option)
+
+if "onu_info" not in st.session_state:
+    st.session_state.onu_info = None
 
 st.set_page_config(page_title="Consulta de ONU/ONT", layout="wide")
 st.title("🔍 Consulta de ONU/ONT")
 
-#search_option = st.radio("Selecione o tipo de consulta:", ('Número do Contrato', 'Número de Série', 'Endereço MAC'), index=0, horizontal=True)
-with st.form(key='search_form'):
-    search_option = st.radio("Selecione o tipo de consulta:", ('Número do Contrato', 'Número de Série', 'Endereço MAC'), index=0, horizontal=True)
-    col1, col2 = st.columns([1,1])
+with st.form(key='search_form', enter_to_submit=True, border=False):
+    search_option = st.radio(
+        "Selecione o tipo de consulta:", 
+        ('Número do Contrato', 'Número de Série', 'Endereço MAC'),
+        index=0, 
+        horizontal=True
+    )
+    col1, col2 = st.columns([1,2])
+
     with col1:
         input_text = st.text_input("Digite a Busca: ")
+
     with col2:
-        st.write("")  # Espaço para alinhar o botão
-        st.write("")  # Espaço para alinhar o botão
+        st.write("")
+        st.write("")
         submit_button = st.form_submit_button(label='Buscar')
 
-if submit_button:
-    match search_option:
-        case 'Número do Contrato':
-            search_type = 'device_alias'
-        case 'Número de Série':
-            search_type = 'serial'
-        case 'Endereço MAC':
-            search_type = 'mac'
+    onu_info = None
 
-    if not input_text:
-        onu_info = None
-        st.error("Por favor, insira um valor para a busca.")
-    else:
-        onu_info = api.get_ont(api.get_ontID(input_text, search_type))
+    if submit_button:
+        if not input_text:
+            st.error("Por favor, insira um valor para a busca.")
+            st.stop()
 
-    onu_info = onu_info['equipment'] if onu_info else None
-    #print(json.dumps(onu_info, indent=3))
-    if onu_info:
-        st.subheader("Infomações da ONU/ONT (OLTCLOUD):")
-        #st.json(onu_info)
-        st.write(f'''
-                 Contrato: {onu_info['device_alias']}<br>
-                 Serial Number: {onu_info['serial_number']}<br>
-                 ONU ID: {onu_info['id']}<br>
-                 MAC: {', '.join(onu_info['macs'])}
-                ''', unsafe_allow_html=True)
-        #st.write(f"Model: {onu_info['model']}")
-        st.subheader("Parametros da ONU/ONT:")
-        st.write(f'''
-                Status: {onu_info['status']}<br>
-                RX Power: {onu_info['device_rx']} dBm<br>
-                TX Power: {onu_info['device_tx']} dBm<br>
-                OLT RX Power: {onu_info['olt_rx']} dBm<br>
-                Temperature: {onu_info['temperature']} °C<br>
-                OLT: {onu_info['olt']}<br>
-                SLOT/PON/ONU ID: {onu_info['slot/pon/onu_id']}<br>
-                VLAN: {', '.join(onu_info['vlans'])}
-        ''', unsafe_allow_html=True)
+        api = get_api()
+        search_type = get_search_type(search_option)
+
+        try:
+            ont_id = api.get_ontID(input_text, search_type)
+            st.session_state.onu_info = api.get_ont(ont_id)
+            st.session_state.onu_info = st.session_state.onu_info.get('equipment') if st.session_state.onu_info else None
+
+        except Exception as e:
+            st.error(f"Erro ao consultar API: {str(e)}")
+            st.stop()
+    
+    if st.session_state.onu_info:
+        st.subheader("📡 Informações da ONU/ONT")
+
+        st.markdown(f"""
+        **Contrato:** {st.session_state.onu_info.get('device_alias').split('-')[0]}  
+        **Serial:** {st.session_state.onu_info.get('serial_number')}  
+        **ONU ID:** {st.session_state.onu_info.get('id')}  
+        **MAC:** {', '.join(st.session_state.onu_info.get('macs', []))}
+        """)
+
+
+        st.subheader("⚙️ Parâmetros")
+        st.markdown(f"""
+        **Status:** {st.session_state.onu_info.get('status')}  
+        **RX:** {st.session_state.onu_info.get('device_rx')} dBm  
+        **TX:** {st.session_state.onu_info.get('device_tx')} dBm  
+        **OLT RX:** {st.session_state.onu_info.get('olt_rx')} dBm  
+        """)
+
+        st.markdown(f"""
+        **Temperatura:** {st.session_state.onu_info.get('temperature')} °C  
+        **OLT:** {st.session_state.onu_info.get('olt')}  
+        **SLOT/PON/ONU:** {st.session_state.onu_info.get('slot/pon/onu_id')}  
+        **VLANs:** {', '.join(st.session_state.onu_info.get('vlans', []))}
+        """)
     else:
-        st.error("Nenhuma ONU/ONT encontrada para o número do contrato informado.")
+        st.warning("Nenhuma ONU/ONT encontrada.")
